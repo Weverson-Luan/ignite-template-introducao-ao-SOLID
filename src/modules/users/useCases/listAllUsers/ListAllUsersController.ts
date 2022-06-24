fdad195a47a4a55c53fbce5e9c4372fbd4247af9
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-06-23 23:31:33 -0300

    feat :boom: feito controller para listagem de todos  user.

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,15 +6,12 @@ import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  async handle(request: Request, response: Response): Promise<Response> {
-    const { user_id } = request.params as { user_id: string };
+   handle(request: Request, response: Response): Response {
+    const { user_id } = request.headers as { user_id: string };
     try {
-      const users = await this.listAllUsersUseCase.execute({ user_id });
+      const users =  this.listAllUsersUseCase.execute({ user_id });
 
-      return response.status(201).json({
-        error: false,
-        data: users,
-      });
+      return response.status(200).json(users);
     } catch (error) {
       return response.status(400).json({
         error: error.message || "Unexpected error.",
