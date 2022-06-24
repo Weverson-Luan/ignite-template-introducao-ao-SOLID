6c356ffe8443ec273702f789f51c0b7e67061e80
Weverson-Luan <weverson.luan@smartinnovation.com.br>
2022-06-23 23:36:03 -0300

    feat :boom: feito controller para tornar um usuário administrador.

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
@@ -5,8 +6,17 @@ import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+   handle(request: Request, response: Response): Response {
+    try {
+      const { user_id } = request.params as { user_id: string };
+      const userAdmin = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(userAdmin);
+    } catch (error) {
+      return response.status(404).json({
+        error: error.message || "Unexpected error.",
+      });
+    }
   }
 }
 
